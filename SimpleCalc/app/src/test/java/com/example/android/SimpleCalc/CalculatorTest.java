9ab491945cbc8acf2867e0f142ca8dd4d25aab18
Prajuk Nusbaum <deomax@yahoo.com>
2020-02-03 20:46:48 -0600

    Completed 3.2 Unit Test
    Signed-off-by: Prajuk Nusbaum <deomax@yahoo.com>

--- SimpleCalc/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
+++ SimpleCalc/app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -25,6 +25,7 @@ import org.junit.runners.JUnit4;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -51,9 +52,17 @@ public class CalculatorTest {
     public void addTwoNumbers() {
         double resultAdd = mCalculator.add(1d, 1d);
         assertThat( resultAdd, is(equalTo(2d)));
-
     }
 
-
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = mCalculator.add(1.111f, 1.111d);
+        assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    }
 
 }
